90143eb4bebf7a3b18b82ff617855b0d2216bcf9
Elanchezhiyan M <chezhiyanmahi2020@gmail.com>
2025-11-09 19:53:03 +0530

    contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -180,7 +180,7 @@ export default function Contact() {
     <div className="min-h-screen">
       {/* Hero Section */}
       {/* <section className="relative overflow-hidden min-h-screen flex items-center bg-gradient-to-br from-slate-50 via-white to-blue-50/30 dark:from-slate-950 dark:via-slate-900 dark:to-blue-950/20"> */}
-      <section className="relative overflow-hidden sm:py-10 md:py-0  min-h-screen flex items-center bg-gradient-to-br from-slate-50 via-white to-blue-50/30 dark:from-slate-950 dark:via-slate-900 dark:to-blue-950/20">
+      <section className="relative overflow-hidden py-2 sm:py-8 md:py-0  min-h-screen flex items-center bg-gradient-to-br from-slate-50 via-white to-blue-50/30 dark:from-slate-950 dark:via-slate-900 dark:to-blue-950/20">
         {/* Modern background grid */}
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
 
